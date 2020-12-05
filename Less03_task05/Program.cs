ac92838606123cb1301f3ad084e33e455f7f248d
Hayk <hayk.hayr3@gmail.com>
2020-12-05 21:56:41 +0400

    Add project files.

---
+++ Less03_task05/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace Less03_task05
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Base obj = new Derived();
+            Console.WriteLine(obj.Method(5));
+
+            Console.ReadKey();
+        }
+    }
+
+    class Base
+    {
+        public virtual string Method(int i)
+        {
+            return "Base " + i;
+        }
+    }
+
+    class Derived : Base
+    {
+        public override string Method(int i)
+        {
+            return base.Method(i) + " derived " + i;
+        }
+    }
+}
